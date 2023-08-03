ce1fe62affa2273ccdfcef49fef7628ebebc2ab5
Jones <Travis.Jones@staples.com>
2023-08-03 13:51:05 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(name='Dread',
       version='1.0',
-      packages=[],
+      packages=['Dread'],
       py_modules=['functions','config','utils','decision_engine','engine_init','engine_build','engine_test'],
       author='Travis Jones',
       author_email = 'travis.jones.610@gmail.com'
