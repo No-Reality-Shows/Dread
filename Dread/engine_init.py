c0206040beba97f62d30864fc9cbc83123e5b976
Jones <Travis.Jones@staples.com>
2023-08-03 14:58:47 -0400

    updating documentation since I dont have any...

--- Dread/engine_init.py
+++ Dread/engine_init.py
@@ -67,25 +67,25 @@ def init(name=None, directory=None):
             print('Directories and template files created for {ENGINE}'.format(ENGINE=engine_name))
             
             steps = """NEXT STEPS:
-                \t1:) Create DataModel by adding entries to 'DataModel/attributes.csv' and 'DataModel/expressions.csv' files
-                \t2:) Create LogicModel by creating ruleset '.csv' files in 'LogicModel/' directory
-                \t\t- A template ruleset file can be found in 'Templates/ruleset_template.csv' directory
-                \t3:) Create DataPipeline by creating data pipeline '.csv' files in 'DataPipelines/' directory
-                \t\t- A template data pipeline file can be found in 'Templates/data_pipeline_template.csv' directory
-                \t\t- Multiple files will result multiple DataPipelines within the rule engine
-                \t4:) Create LogicPipeline by creating logic pipeline '.csv' files in 'LogicPipelines/' directory
-                \t\t- A template data pipeline file can be found in 'Templates/data_pipeline_template.csv' directory
-                \t\t- Multiple files will result multiple LogicPipelines within the rule engine
-                \t5:) From python, build decision engine which will create the decision engine file {ENGINE}.pkl
-                \t\t"from Dread.engine_build import build"
-                \t\t"build('{ENGINE}')"
-                \tYay, your first decision engine was built, now it's time to test it.
-                \t6:)From python, test rule engine against test files in the TestData directory
-                \t\t- 'Dread.engine_test.test' requires JSON formatted data which is read into a python dictionary object which the engine accepts
-                \t\t- The outputs from the test execution will be placed in the /TestData/TestResults direcotry
-                \t\t"from Dread.engine_test import test"
-                \t\t"test('{ENGINE}')"
-                """.format(ENGINE = engine_name)
+\t1:) Create DataModel by adding entries to 'DataModel/attributes.csv' and 'DataModel/expressions.csv' files
+\t2:) Create LogicModel by creating ruleset '.csv' files in 'LogicModel/' directory
+\t\t- A template ruleset file can be found in 'Templates/ruleset_template.csv' directory
+\t3:) Create DataPipeline by creating data pipeline '.csv' files in 'DataPipelines/' directory
+\t\t- A template data pipeline file can be found in 'Templates/data_pipeline_template.csv' directory
+\t\t- Multiple files will result multiple DataPipelines within the rule engine
+\t4:) Create LogicPipeline by creating logic pipeline '.csv' files in 'LogicPipelines/' directory
+\t\t- A template data pipeline file can be found in 'Templates/data_pipeline_template.csv' directory
+\t\t- Multiple files will result multiple LogicPipelines within the rule engine
+\t5:) From python, build decision engine which will create the decision engine file {ENGINE}.pkl
+\t\t"from Dread.engine_build import build"
+\t\t"build('{ENGINE}')"
+\tYay, your first decision engine was built, now it's time to test it.
+\t6:)From python, test rule engine against test files in the TestData directory
+\t\t- 'Dread.engine_test.test' requires JSON formatted data which is read into a python dictionary object which the engine accepts
+\t\t- The outputs from the test execution will be placed in the /TestData/TestResults direcotry
+\t\t"from Dread.engine_test import test"
+\t\t"test('{ENGINE}')"
+""".format(ENGINE = engine_name)
             print(steps)
             #write steps to file
             with open('/'.join([build_dir, engine_name,'README.txt']), 'w') as f:
